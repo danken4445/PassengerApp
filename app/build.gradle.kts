6094441b8090ff4c20ab003801c8f9cdf618495f
Dan Ken Shen Penera <danken4445@gmail.com>
2023-09-02 05:12:36 +0800

    Improved UI and Changed Logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,14 @@
+import org.gradle.util.GradleVersion.version
+
 plugins {
     id("com.android.application")
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
-
 }
-
 
 android {
     namespace = "com.example.passengeractivity"
     compileSdk = 33
+    buildToolsVersion = "34.0.0"
 
     defaultConfig {
         applicationId = "com.example.passengeractivity"
@@ -15,9 +16,9 @@ android {
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
 
     buildTypes {
         release {
@@ -25,6 +26,7 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -32,22 +34,16 @@ android {
 }
 
 dependencies {
-
-
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.9.0") // Use consistent version
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.android.volley:volley:1.2.1")
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("com.karumi:dexter:6.2.2")
-    implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("androidx.core:core:1.7.0")
+    implementation("androidx.core:core:1.7.0") // Use androidx.core instead of androidx.appcompat
     implementation("com.airbnb.android:lottie:4.0.0")
     implementation("com.google.zxing:core:3.4.1")
-
-
 }
